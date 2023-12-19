ffc5b46db8e4d1c5f486f5752b2d0768f68f1dfd
Saurabh-Mahadik <128701148+Saurabh-Mahadik@users.noreply.github.com>
2023-12-19 13:41:06 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,19 +1,17 @@
 <?php
 $host = 'localhost';
 $user = 'root';
-$password = ''; // Replace with your actual password
+$password = 'saurabh@123'; 
 $database = 'saurabh';
 
-// Create a connection to the MySQL database
 $conn = mysqli_connect($host, $user, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-echo "Connected successfully";
+echo "Connection successfully";
 
-// Now you can perform database operations using $conn
 
-// Remember to close the connection when you're done
+
 mysqli_close($conn)
 ?>
